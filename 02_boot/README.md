2a57a156e7d54954c416a67ee4df81cb85f72cbe
honey <honey634546@cug.edu.cn>
2020-05-20 20:03:14 +0800

    --story=1002409 --user=honey

---
+++ 02_boot/README.md
@@ -0,0 +1,22 @@
+com.example.boot
+
+- controller ：控制层
+- dao : 存放操作数据库的接口O(持久层)
+- dto : 存放数据传输对象，比如注册时，注册的信息可以用一个 dto 对象来接收
+- po : 存放与数据库中表相对应的java对象
+- qo : 封装了查询参数的对象
+- service : 业务层
+- vo : 存放业务返回结果对象
+
+
+resources
+
+- mapper : 存放mapper.xml 文件
+
+用到的插件：
+mybatis
+tk.mybatis
+mybatis generator
+pagehelper
+lombok
+
---
+++ 02_boot/boot-user/src/main/java/com/example/boot/user/dto/RegisterDTO.java
@@ -0,0 +1,34 @@
+package com.example.boot.user.dto;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @Author : wang ziyu
+ * @create 2020/3/22 4:19
+ */
+@Data
+public class RegisterDTO {
+
+    /**
+     * 用户名
+     */
+    @NotBlank(message = "用户名不能为空")
+    private String userName;
+
+    /**
+     * 密码
+     */
+    @Length(min = 8, max = 20, message = "密码必须是8至20位")
+    private String password;
+
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+}
